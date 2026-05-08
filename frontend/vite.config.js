066dd7e3969bffc591bb979150a9d1c8087fd7d8
Abdulakhad <turgunalievabdulahad6554@gmail.com>
2026-05-07 22:35:40 -0400

    Bismillah, vite.config.js has been changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/menu': 'https://restaurant-dynamic-platform.onrender.com',
-      '/uploads': 'https://restaurant-dynamic-platform.onrender.com',
-    },
   },
 });
